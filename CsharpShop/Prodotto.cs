90b78f031577cf7651cd351d3b7800f82aeb8a87
Gennaro <gennarinodi02@gmail.com>
2022-04-08 16:11:57 +0200

    2 commit

---
+++ CsharpShop/Prodotto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpShop
+{
+    internal class Prodotto
+    {
+
+        private int Codice { get; }
+        private string nome;
+        private string descrizione;
+        private double prezzo;
+        private double iva;
+
+
+        public Prodotto( string nome, string descrizione, double prezzo) //Costruttore 1
+        {
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+        }
+
+        public Prodotto(int Codice, string nome, string descrizione, double prezzo, double iva) //Costruttore Completo
+        {
+            this.Codice = Codice;
+            this.nome = nome;   
+            this.descrizione = descrizione; 
+            this.prezzo = prezzo;   
+            this.iva = iva; 
+        }
+
+        public int  CreaCodiceProdotto()
+        {
+            Random Codice = new Random();
+            int CodiceProdotto = Codice.Next(99999999);
+            return CodiceProdotto;  
+        }
+
+       /* public double PrezzoBase()
+        {
+            Random prezzo = new Random();
+          double PrezzoB = prezzo.Next(100);
+
+            return PrezzoB;
+         }
+       */
+            
+        public double CalcolaPrezzoIva( double prezzo)
+        { 
+           
+            double IVA = prezzo * 22 / 100;
+            double PrezzoIVA = prezzo + IVA;
+                return PrezzoIVA;
+        }
+
+         public void StampaProdottoIntero()
+        {
+            Console.WriteLine("---------- PRODOTTO ----------");
+            Console.WriteLine("CODE: " +CreaCodiceProdotto());
+            Console.WriteLine("NOME: " +nome);
+            Console.WriteLine("Descrizione: " +descrizione) ;
+            Console.WriteLine("PREZZO (NO IVA): " + prezzo);
+            Console.WriteLine("PREZZO (IVA): " +CalcolaPrezzoIva(prezzo));
+
+            Console.WriteLine("------------------------------");
+        }
+
+        
+
+
+
+
+
+
+
+    }
+}
--- CsharpShop/Program.cs
+++ CsharpShop/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using CsharpShop;
+string risposta;
+bool ciclo = true;
+while ( true)
+{
+    Console.WriteLine("inserisci Il nome del prodotto:");
+    string NomeProdotto = Console.ReadLine();
+
+    Console.WriteLine("inserisci la descrizine del prodotto:");
+    string DescrizioneProdotto = Console.ReadLine();
+
+    Console.WriteLine("inserisci Il Prezzo del prodotto:");
+    double PrezzoProdotto = double.Parse(Console.ReadLine());
+
+
+    Prodotto Prodotto1 = new Prodotto(NomeProdotto, DescrizioneProdotto, PrezzoProdotto);
+
+    Prodotto1.CalcolaPrezzoIva(PrezzoProdotto);
+    Prodotto1.StampaProdottoIntero();
+
+    Console.WriteLine("PREMI (F) per NON inserire più i prodotti Oppure qualsiasi input per continuare");
+    risposta = Console.ReadLine().ToLower();
+    if (risposta == "f")
+    {
+         ciclo=false;
+    }
+       
+}
